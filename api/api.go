e5ba8f4ce58dfabab31aed49b221afa5524c7f7b
thosaphol <thosaphol@outlook.co.th>
2024-05-18 15:54:10 +0700

    [INIT] transaction package

--- api/api.go
+++ api/api.go
@@ -8,6 +8,7 @@ import (
 	"github.com/KKGo-Software-engineering/workshop-summer/api/health"
 	"github.com/KKGo-Software-engineering/workshop-summer/api/mlog"
 	"github.com/KKGo-Software-engineering/workshop-summer/api/spender"
+	"github.com/KKGo-Software-engineering/workshop-summer/api/transaction"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"go.uber.org/zap"
@@ -29,12 +30,16 @@ func New(db *sql.DB, cfg config.Config, logger *zap.Logger) *Server {
 	v1.GET("/health", health.Check(db))
 	v1.POST("/upload", eslip.Upload)
 
+	handleE := transaction.New(cfg.FeatureFlag, db)
+	v1.GET("/expenses", handleE.GetAll)
+
 	v1.Use(middleware.BasicAuth(AuthCheck))
 
 	{
 		h := spender.New(cfg.FeatureFlag, db)
 		v1.GET("/spenders", h.GetAll)
 		v1.POST("/spenders", h.Create)
+
 	}
 
 	return &Server{e}
---
+++ api/transaction/transaction.go
@@ -0,0 +1,63 @@
+package transaction
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/KKGo-Software-engineering/workshop-summer/api/config"
+	"github.com/KKGo-Software-engineering/workshop-summer/api/mlog"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+type Expense struct {
+	ID              int64   `json:"id"`
+	Date            string  `json:"date"`
+	Amount          float64 `json:"amount"`
+	Category        string  `json:"category"`
+	TransactionType string  `json:"transactionType"`
+	Note            string  `json:"note"`
+	ImageURL        string  `json:"imageUrl"`
+	SpenderId       int64   `json:"spenderId"`
+}
+
+type handler struct {
+	flag config.FeatureFlag
+	db   *sql.DB
+}
+
+func New(cfg config.FeatureFlag, db *sql.DB) *handler {
+	return &handler{cfg, db}
+}
+
+const (
+	cStmt = `INSERT INTO spender (name, email) VALUES ($1, $2) RETURNING id;`
+)
+
+func (h handler) GetAll(c echo.Context) error {
+	logger := mlog.L(c)
+	ctx := c.Request().Context()
+
+	/*
+	 */
+
+	rows, err := h.db.QueryContext(ctx, `SELECT * FROM transaction WHERE transaction_type='expense'`)
+	if err != nil {
+		logger.Error("query error", zap.Error(err))
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	defer rows.Close()
+
+	var exs []Expense
+	for rows.Next() {
+		var ex Expense
+		err := rows.Scan(&ex.ID, &ex.Date, &ex.Amount, &ex.Category, &ex.TransactionType, &ex.Note, &ex.ImageURL, &ex.SpenderId)
+		if err != nil {
+			logger.Error("scan error", zap.Error(err))
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		exs = append(exs, ex)
+	}
+
+	return c.JSON(http.StatusOK, exs)
+}
