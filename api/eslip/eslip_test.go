b6fef04e280136c7c47f1e6231de93b55533fafc
h3xry <h3xryx3+github@gmail.com>
2024-05-18 20:52:21 +0700

    add test upload

--- api/eslip/eslip_test.go
+++ api/eslip/eslip_test.go
@@ -1,9 +1,13 @@
 package eslip
 
 import (
+	"bytes"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo/v4"
@@ -29,4 +33,30 @@ func TestUploadToS3(t *testing.T) {
 		assert.Equal(t, "location/on/s3/bucket/eslip1.jpg", loc)
 	})
 
+}
+
+func TestUpload(t *testing.T) {
+	t.Run("should able to upload image", func(t *testing.T) {
+		e := echo.New()
+		defer e.Close()
+
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		part, err := writer.CreateFormFile("images", "test.jpg")
+		assert.NoError(t, err)
+		_, err = io.Copy(part, strings.NewReader("fake image content"))
+		assert.NoError(t, err)
+		writer.Close()
+
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, writer.FormDataContentType())
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		Upload(c)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Body.String(), "Image uploaded successfully")
+	})
 }
